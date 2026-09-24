2bb0d8de0c78b8d27068d6243ed9689dbba4e6d2
sakada <ahmok4081@gmail.com>
2026-09-24 23:27:03 +0700

    list review

---
+++ list_and_array/list.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "orange", "banana", "coconunt"]
+print(fruits[:3])
+print([fruits[::-1]])
+print([fruits[::2]])
+#or fruit in fruits:ß
+ #   print(fruit)
+print(fruits.count("banana"))
+fruits.append("sakada like vatey")
+fruits.remove('sakada like vatey')
+fruits[0] = "kdmv"
+fruits.sort()
+fruits.clear()
+print(fruits)
