48542fba49d2690a8bfd2f6ddf9498fc97ada193
sakada <ahmok4081@gmail.com>
2026-09-25 22:23:21 +0700

    list review 2

--- list_and_array/list.py
+++ list_and_array/list.py
@@ -1,9 +1,17 @@
+#list: An ordered, mutable collection that allows duplicate elements. Created using square brackets []
+# .tuple: An ordered, immutable (unchangeable) collection that allows duplicate elements. Created using parentheses ().
+# set: An unordered, mutable collection of unique elements (no duplicates allowed). Created using curly braces {} or set().
+# dict: An ordered (as of Python 3.7), mutable collection of key-value pairs. Keys must be unique.
+#  Created using curly braces with colons {'key': 'value'}
+#lidt can be mix with mutiple datatype
 fruits = ["apple", "orange", "banana", "coconunt"]
 print(fruits[:3])
 print([fruits[::-1]])
 print([fruits[::2]])
-#or fruit in fruits:ß
+#or fruit in fruits:ßß
  #   print(fruit)
+sakada = list("adafa")
+print(sakada)
 print(fruits.count("banana"))
 fruits.append("sakada like vatey")
 fruits.remove('sakada like vatey')
